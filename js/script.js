503e17dc9e9d0120f8c3052d0d6de5de535fc2e0
Sohail Khan <sohail5412@example.com>
2024-04-19 20:35:19 -0400

    implemented a div for displaying the data

--- js/script.js
+++ js/script.js
@@ -13,6 +13,19 @@ function getData() {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      outputHTML = `
+                <div class="city">
+                    <h2>${data[0].name}</h2>
+                    <p>Latitude: ${data[0].lat}</p>
+                    <p>Longitude: ${data[0].lon}</p>
+                    <p>Country: ${data[0].country}</p>
+                    <p>State: ${data[0].state}</p>
+                    <button onclick="getAQI('${data[0].name}')">Get AQI</button>
+                    <div id="aqi-output" style="float: right"></div>
+                </div>
+            `;
+
+      document.getElementById("output").innerHTML = outputHTML;
     })
     .catch((error) => console.error("Error fetching data:", error));
 }
